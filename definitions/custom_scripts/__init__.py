5602226e141deb5999cb8aa2d6da15e4f3d1f55e
justanothercatgirl <sotov@twistea.su>
2025-11-15 00:40:43 +0300

    Restored accidentally removed __all__ declaration

--- definitions/custom_scripts/__init__.py
+++ definitions/custom_scripts/__init__.py
@@ -6,5 +6,18 @@ from .render import (
     render_drop_group,
     render_drop_sequence,
 )
-from .schemas import add_table_schema_to_model, compare_for_groups
+from .schemas import add_table_schema_to_model, compare_for_groups, compare_schemas
 from .tables import compare_for_encrypted, compare_for_sensitive
+
+__all__ = [
+    "create_table_schema",
+    "drop_table_schema",
+    "render_create_sequence",
+    "render_drop_sequence",
+    "add_table_schema_to_model",
+    "compare_schemas",
+    "create_group",
+    "delete_group",
+    "render_create_group",
+    "render_drop_group",
+]
